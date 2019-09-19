7b659f97c68385cae5d30140986ce4672e3e84c8
Daniel Pinna <daniel.pinna@rededor.com.br>
2019-09-19 10:16:40 -0300

    Finalize layout of login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, View, Text, ImageBackground, Image } from 'react-native';
+import { Item, Input } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Login extends Component {
 
@@ -19,7 +21,22 @@ export default class Login extends Component {
                     </View>
 
                     <View style={ styles.containerForm }>
-                        <Text>Teste</Text>
+
+                        <View style={ styles.inputGroup }>
+                            <Text style={ styles.textLabel }>Usuário</Text>
+                            <Item>
+                                <Icon type='Feather' active name='user' color="#FFFFFF" size={25} />
+                                <Input style={ styles.textInput } placeholder='seu.usuario' placeholderTextColor="#FFFFFF" />
+                            </Item>
+                        </View>
+
+                        <View style={ styles.inputGroup }>
+                            <Text style={ styles.textLabel }>Senha</Text>
+                            <Item>
+                                <Icon type='Feather' active name='lock' color="#FFFFFF" size={25} />
+                                <Input style={ styles.textInput } placeholder='******' placeholderTextColor="#FFFFFF" />
+                            </Item>
+                        </View>
                     </View>
 
                     <View  style={ styles.containerButton }>
@@ -52,11 +69,35 @@ const styles = {
     },
     containerForm: {
         width: '75%',
-        height: '50%',
+        height: '35%',
         marginTop: '15%',
         marginLeft: '12.5%',
         borderColor: 'black',
-        borderWidth: 1,
+        borderWidth: 0,
+    },
+    inputGroup: {
+        marginTop: '5%',
+        marginBottom: '15%',
+    },
+    textLabel: {
+        fontFamily: "Roboto-Bold",
+        fontSize: 14,
+        fontWeight: "bold",
+        fontStyle: "normal",
+        letterSpacing: 0,
+        textAlign: "left",
+        color: "#ffffff"
+    },
+    textInput: {
+        marginLeft: '3%',
+        opacity: 0.6,
+        fontFamily: "Roboto",
+        fontSize: 14,
+        fontWeight: "300",
+        fontStyle: "normal",
+        lineHeight: 14,
+        letterSpacing: 0.02,
+        color: "#ffffff"
     },
     containerButton: {
         marginTop: '10%',
