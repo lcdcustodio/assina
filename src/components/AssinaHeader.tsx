1f6b21b4e246bf97d8ba77834120cf44a822ee3c
oberdan <oberdan@alis-sol.com.br>
2019-11-27 17:04:12 -0300

    Bug de cabeçalho com título resolvido:
    - Título estava numa camada acima e bloqueava os eventos.

--- src/components/AssinaHeader.tsx
+++ src/components/AssinaHeader.tsx
@@ -95,6 +95,7 @@ export const styles = StyleSheet.create({
   } as ViewStyle,
 
   centerContainer: {
+    zIndex: -1,
     position: 'absolute',
     top: 0, right: 0, bottom: 0, left: 0,
     flexDirection: 'row',
